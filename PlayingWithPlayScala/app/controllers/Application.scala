42101f5f42c6bdefa6c6fc51d0019c9a3bbed673
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2016-07-09 21:16:15 -0700

    First flow to display message in browser. Two ways presented one using html and the other using scala method. Note the difference in rendering.

--- PlayingWithPlayScala/app/controllers/Application.scala
+++ PlayingWithPlayScala/app/controllers/Application.scala
@@ -1,12 +1,20 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Test message")
   }
 
+  def sayMessage(name: String) = Action {
+    Ok("Hi " + name)
+  }
+}
+
+object ApplicationNew extends Controller {
+  def sayMessage(name: String) = Action {
+    Ok(views.html.sayMessage(name))
+  }
 }
